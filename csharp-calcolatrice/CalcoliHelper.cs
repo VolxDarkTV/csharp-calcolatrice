34dddaac935d7a05c5260f67ec9cf2087ef94834
Vincenzo FOTI <vincenzo.foti@outlook.it>
2023-04-12 14:15:40 +0200

    Add project files.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal static class CalcoliHelper
+    {
+
+        public static int SommaIntNumeri(int n1, int n2)
+        {
+           return n1 + n2;
+        }
+
+        public static double SommaDoubleNumeri(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int DiffIntNum(int n1, int n2)
+        {
+            //Non aggiungo controlli perché potrebbe servirmi un risultato negativo come nelle calcolatrici scientifiche
+            return n1 - n2;
+        }
+
+        public static double DiffDoubleNum(double n1, double n2)
+        {
+            //Non aggiungo controlli perché potrebbe servirmi un risultato negativo come nelle calcolatrici scientifiche
+            return n1 - n2;
+        }
+
+        public static int MoltIntNum(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+        public static double MoltDoubleNum(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public static int AssoluteIntNum(int n1)
+        {
+            if (n1 < 0)
+            {
+                int result = n1 * (-1);
+                return result;
+            }
+            else
+            {
+                Console.WriteLine(n1);
+                return n1;
+            }
+        }
+
+        public static double AssoluteDoubleNum(double n1)
+        {
+            if (n1 < 0)
+            {
+                double result = n1 * (-1);
+                return result;
+            }
+            else
+            {
+                Console.WriteLine(n1);
+                return n1;
+            }
+        }
+
+        public static int MinIntNum(int n1, int n2)
+        {
+            if(n1 < n2)
+            {
+                return n1;
+            }
+            else if(n1 > n2)
+            {
+                return n2;
+            }
+            else
+            {
+                return n1;
+            }
+        }
+
+        public static double MinDoubleNum(double n1, double n2)
+        {
+            if (n1 < n2)
+            {
+                return n1;
+            }
+            else if (n1 > n2)
+            {
+                return n2;
+            }
+            else
+            {
+                return n1;
+            }
+        }
+        public static int MaxIntNum(int n1, int n2)
+        {
+            if (n1 > n2)
+            {
+                return n1;
+            }
+            else if (n1 < n2)
+            {
+                return n2;
+            }
+            else
+            {
+                return n1;
+            }
+        }
+        public static double MaxDoubleNum(double n1, double n2)
+        {
+            if (n1 > n2)
+            {
+                return n1;
+            }
+            else if (n1 < n2)
+            {
+                return n2;
+            }
+            else
+            {
+                return n1;
+            }
+        }
+
+    }
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace csharp_calcolatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Somma Int = {CalcoliHelper.SommaIntNumeri(5, 6)}");
+
+            Console.WriteLine($"Somma Double = {CalcoliHelper.SommaDoubleNumeri(5.555, 6)}");
+
+            Console.WriteLine($"Diff Int = {CalcoliHelper.DiffIntNum(5, 6)}");
+
+            Console.WriteLine($"Diff Double = {CalcoliHelper.DiffDoubleNum(5.555, 6)}");
+
+            Console.WriteLine($"Molt Int = {CalcoliHelper.MoltIntNum(4, 2)}");
+
+            Console.WriteLine($"Molt Double = {CalcoliHelper.MoltDoubleNum(4, 0.5)}");
+
+            //Valore assoluto Int
+            Console.WriteLine($"Assolute Int = {CalcoliHelper.AssoluteIntNum(CalcoliHelper.DiffIntNum(5, 6))}");
+
+            //Valore assoluto Double
+            Console.WriteLine($"Assolute Double = {CalcoliHelper.AssoluteDoubleNum(CalcoliHelper.DiffDoubleNum(2.5, 9.2))}");
+
+            //Min Int
+            Console.WriteLine($"Min Int = {CalcoliHelper.MinIntNum(2, 2)}");
+
+            //Min Double
+            Console.WriteLine($"Min Double = {CalcoliHelper.MinDoubleNum(2.1, 2.3)}");
+
+            //Max Int
+            Console.WriteLine($"Max Int = {CalcoliHelper.MaxDoubleNum(2.1, 2.3)}");
+
+            //Max Double
+            Console.WriteLine($"Max Double = {CalcoliHelper.MaxDoubleNum(2.7, 6.9)}");
+
+        }
+    }
+}
